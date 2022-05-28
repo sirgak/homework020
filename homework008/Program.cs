b4a5996591f22618c3fe3f7cc505c76940073f96
sirgak <life_4_ever@mail.ru>
2022-05-28 21:42:38 +0300

    test
    e

---
+++ homework008/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace homework008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            int howManyTimes;
+            Console.Write("Введите текст: ");
+            text = Convert.ToString(Console.ReadLine());
+            Console.Write("Количество повторов: ");
+            howManyTimes = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < howManyTimes; i++)
+            {
+                Console.WriteLine(text);
+            }
+        }
+        }       
+    }
+}
